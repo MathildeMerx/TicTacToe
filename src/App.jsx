ca57225c19a3fc7fd24b36329574d9795094fb3a
MathildeMerx <mathilde.merx0@gmail.com>
2022-01-24 09:39:55 +0100

    back to only tic tac toe with no history

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import "./App.css";
 import { useState } from "react";
-import {
-    BoardCell,
-    computeGameOutcome,
-    computeNextPlayer,
-    computeCurrentStep,
-} from "./cells.js";
+import { BoardCell, computeGameOutcome, computeNextPlayer } from "./cells.js";
 import "@fontsource/roboto/300.css";
 import Button from "@mui/material/Button";
 import RestartAltIcon from "@mui/icons-material/RestartAlt";
@@ -15,80 +10,39 @@ function App() {
     const nextPlayer = computeNextPlayer(board);
     const gameOutcome = computeGameOutcome(board);
 
-    const [boardHistory, setBoardHistory] = useState(
-        Array(10).fill(Array(9).fill(null))
-    );
-    const currentStep = computeCurrentStep(boardHistory);
-    console.log(`From main app: boardHistory ${boardHistory}`);
-    const nextPlayerHistory = computeNextPlayer(boardHistory[currentStep]);
-    const gameOutcomeHistory = computeGameOutcome(boardHistory[currentStep]);
-
     return (
-        <>
-            <div className="game">
-                <p className="game-info">
-                    {gameOutcome
-                        ? gameOutcome === "draw"
-                            ? `Game over: draw`
-                            : `Game over, ${gameOutcome} wins!`
-                        : `Next player: ${nextPlayer}`}
-                </p>
-                <div className="game-board">
-                    {[...Array(9).keys()].map((e) => (
-                        <BoardCell
-                            index={e}
-                            board={board}
-                            setBoard={setBoard}
-                            nextPlayer={nextPlayer}
-                            gameOutcome={gameOutcome}
-                            history={false}
-                        />
-                    ))}
-                </div>
-                <div className="restart-button">
-                    <Button
-                        onClick={() => setBoard(Array(9).fill(null))}
-                        variant="outlined"
-                        startIcon={<RestartAltIcon />}
-                    >
-                        Restart
-                    </Button>
-                </div>
+        <div className="game">
+            <p className="game-info">
+                {gameOutcome
+                    ? gameOutcome === "draw"
+                        ? `Game over: draw`
+                        : `Game over, ${gameOutcome} wins!`
+                    : `Next player: ${nextPlayer}`}
+            </p>
+            <div className="game-board">
+                {[...Array(9).keys()].map((e) => (
+                    <BoardCell
+                        index={e}
+                        board={board}
+                        setBoard={setBoard}
+                        nextPlayer={nextPlayer}
+                        gameOutcome={gameOutcome}
+                    />
+                ))}
             </div>
-            <div className="game">
-                <p className="game-info">
+            <div className="restart-button">
+                {/* <Button onClick={() => setBoard(Array(9).fill(null))}>
                     {" "}
-                    Game with history.{" "}
-                    {gameOutcome
-                        ? gameOutcome === "draw"
-                            ? `Game over: draw`
-                            : `Game over, ${gameOutcome} wins!`
-                        : `Next player: ${nextPlayer}`}
-                </p>
-                <div className="game-board">
-                    {[...Array(9).keys()].map((e) => (
-                        <BoardCell
-                            index={e}
-                            board={boardHistory}
-                            setBoard={setBoardHistory}
-                            nextPlayer={nextPlayerHistory}
-                            gameOutcome={gameOutcomeHistory}
-                            history={true}
-                            currentStep={currentStep}
-                        />
-                    ))}
-                </div>
-                <div className="restart-button">
-                    <Button
-                        onClick={() => setBoard(Array(9).fill(null))}
-                        variant="outlined"
-                        startIcon={<RestartAltIcon />}
-                    >
-                        Restart
-                    </Button>
-                </div>
+                    Restart{" "} */}
+                <Button
+                    onClick={() => setBoard(Array(9).fill(null))}
+                    variant="outlined"
+                    startIcon={<RestartAltIcon />}
+                >
+                    Restart
+                </Button>
             </div>
-        </>
+        </div>
     );
 }
 
--- src/cells.js
+++ src/cells.js
@@ -1,37 +1,12 @@
 import "./App.css";
 
-function BoardCell({
-    index,
-    board,
-    setBoard,
-    nextPlayer,
-    gameOutcome,
-    history,
-    currentStep,
-}) {
+function BoardCell({ index, board, setBoard, nextPlayer, gameOutcome }) {
     return (
         <button
             variant="outlined"
             className="board-cell"
-            onClick={
-                !history
-                    ? () =>
-                          cellClick(
-                              index,
-                              board,
-                              setBoard,
-                              nextPlayer,
-                              gameOutcome
-                          )
-                    : () =>
-                          cellClickHistory(
-                              index,
-                              board,
-                              setBoard,
-                              nextPlayer,
-                              gameOutcome,
-                              currentStep
-                          )
+            onClick={() =>
+                cellClick(index, board, setBoard, nextPlayer, gameOutcome)
             }
         >
             {board[index]}
@@ -48,30 +23,7 @@ function cellClick(i, board, setBoard, nextPlayer, gameOutcome) {
     }
 }
 
-function cellClickHistory(
-    i,
-    board,
-    setBoard,
-    nextPlayer,
-    gameOutcome,
-    currentStep
-) {
-    if (gameOutcome || board[currentStep][i]) {
-        return;
-    } else {
-        const copyBoard = Array(board.length).fill(null);
-        for (let j = 0; j < board.length; j++) {
-            copyBoard[j] = [...board[j]];
-        }
-        copyBoard[currentStep + 1][i] = nextPlayer;
-        setBoard(copyBoard);
-    }
-}
-
 function computeNextPlayer(board) {
-    console.log(
-        `The first element of board is: ${board[0]}, and the board is ${board}`
-    );
     return board.filter((x) => x === null).length % 2 === 0 ? "O" : "X";
 }
 
@@ -98,8 +50,4 @@ function computeGameOutcome(board) {
     return null;
 }
 
-function computeCurrentStep(boardHistory) {
-    return 10 - boardHistory.filter((e) => e.every((x) => x === null)).length;
-}
-
-export { BoardCell, computeGameOutcome, computeNextPlayer, computeCurrentStep };
+export { BoardCell, computeGameOutcome, computeNextPlayer };
