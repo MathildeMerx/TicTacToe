e6abbf916ab88a4a6fd99314ff4d7ef8a1e20a14
MathildeMerx <mathilde.merx0@gmail.com>
2022-05-11 22:31:52 +0200

    other test to fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,39 @@
 import "./App.css";
 import { Link } from "react-router-dom";
-import { HeadProvider } from "react-head";
 
 function App() {
     return (
-        <HeadProvider>
-            <meta
-                http-equiv="Content-Security-Policy"
-                content="upgrade-insecure-requests"
-            ></meta>
-            <div>
-                <header className="title">
-                    <h1>Welcome to Matilda's website!</h1>
-                </header>
-                <main className="main-content index-page">
-                    <section className="subtitle">
-                        <h2>
-                            Here are my apps! Feel free to explore each page...
-                            and have fun :)
-                        </h2>
-                    </section>
-                    <div className="page-link">
-                        <Link className="page-link" to="/tic_tac_toe">
-                            {" "}
-                            Tic Tac Toe game
-                        </Link>
-                    </div>
-                    <section>
-                        A tic tac toe game, with a history of the ongoing game.
-                        Closed the tab by mistake? No worries: your game is
-                        locally stored!
-                    </section>
-                    <div className="page-link">
-                        <Link to="/button_maker"> Button maker</Link>
-                    </div>
-                    <section>
-                        A button maker, in which you can customize size, color,
-                        shape, and many other styles! Implementation in
-                        progress.
-                    </section>
-                </main>
-            </div>
-        </HeadProvider>
+        <div>
+            <header className="title">
+                <h1>Welcome to Matilda's website!</h1>
+            </header>
+            <main className="main-content index-page">
+                <section className="subtitle">
+                    <h2>
+                        Here are my apps! Feel free to explore each page... and
+                        have fun :)
+                    </h2>
+                </section>
+                <div className="page-link">
+                    <Link className="page-link" to="/tic_tac_toe">
+                        {" "}
+                        Tic Tac Toe game
+                    </Link>
+                </div>
+                <section>
+                    A tic tac toe game, with a history of the ongoing game.
+                    Closed the tab by mistake? No worries: your game is locally
+                    stored!
+                </section>
+                <div className="page-link">
+                    <Link to="/button_maker"> Button maker</Link>
+                </div>
+                <section>
+                    A button maker, in which you can customize size, color,
+                    shape, and many other styles! Implementation in progress.
+                </section>
+            </main>
+        </div>
     );
 }
 
